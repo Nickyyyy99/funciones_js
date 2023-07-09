e30c32212425e2af1b11134d4984d174ed4ac384
nicole <125519306+Nickyyyy99@users.noreply.github.com>
2023-07-09 19:47:03 -0300

    Add files via upload

--- ejercicios_profundizacion/js/profundizacion_1.js
+++ ejercicios_profundizacion/js/profundizacion_1.js
@@ -12,3 +12,23 @@
  *    Debe recibir 5 números como argumento y calcular el promedio.  Debe reportar por
  *    medio de alert(); y console.log(); el detalle de los datos recibidos.
 */
+
+function promedio (a,b,c,d,e) {
+    let suma = a + b + c + d + e
+    let promedio = suma / 5
+
+    let datos = "Datos: " + a + ","+ b  + "," + c + "," + d + ","+ e;
+    alert(datos)
+    console.log(datos)
+
+    alert("El promedio es: " + promedio);
+    
+    console.log("El promedio es: " + promedio);
+
+    return promedio
+} 
+
+let resultado = promedio(1,23,7,4,22)
+console.log("Resultado: " + resultado)
+
+  
--- ejercicios_profundizacion/js/profundizacion_2.js
+++ ejercicios_profundizacion/js/profundizacion_2.js
@@ -1,15 +1,46 @@
-/**
- * @fileoverview Ejercitación de Funciones
- * @author       Inove coding School <alumnos@inove.com.ar>
- * @copyright    www.inove.com.ar
- *
- * Consignas:
- * Deberá enlazar este archivo javascript al documento HTML.
- * Del ejercicio visto en clase, realizar los cambios necesarios para que el sistema
- * admita:
- * 
- * 2. En la función generarInvitados():
- *    Crear una variable global que indique si se debe solicitar 1, 2 o 3 nombres de invitados.
- *    Según la cantidad especificada utilice condicionales para solicitar la cantidad de nombres
- *    correctos.
-*/
+
+//  * @fileoverview Ejercitación de Funciones
+//  * @author       Inove coding School <alumnos@inove.com.ar>
+//  * @copyright    www.inove.com.ar
+//  *
+//  * Consignas:
+//  * Deberá enlazar este archivo javascript al documento HTML.
+//  * Del ejercicio visto en clase, realizar los cambios necesarios para que el sistema
+//  * admita:
+//  * 
+//  * 2. En la función generarInvitados():
+//  *    Crear una variable global que indique si se debe solicitar 1, 2 o 3 nombres de invitados.
+//  *    Según la cantidad especificada utilice condicionales para solicitar la cantidad de nombres
+//  *    correctos. 
+
+//variable global que indica 3 nombres 
+let cantidadNombres = alert("Ingrese tres invitados")
+
+ function generarInvitados () {
+     if (cantidadNombres == 1) {
+         let nombre1 = prompt("Ingrese el nombre del primer invitado: ")
+         console.log("Nombre del invitado nro1: " + nombre1)
+     } 
+     else if (cantidadNombres == 2) {
+         let nombre2 = prompt("Ingrese el nombre del segundo invitado: ")
+         console.log("Nombre del invitado nro2: "+ nombre2)
+     }
+     else if (cantidadNombres == 3) {
+         let nombre3 = prompt("Ingrese el nombre del tercer invitado")
+         console.log("Nombre del invitado nro3: " + nombre3)
+     }
+     else {
+         console.log("Cantidad de invitados no valida")
+     }
+     
+ }
+cantidadNombres = 1
+generarInvitados()
+cantidadNombres = 2
+generarInvitados()
+cantidadNombres =3
+generarInvitados()
+
+
+
+  
